b5bcfd3a486ba24056b2af5a80b4762fd5cfeed4
tipowers <tpowers88@outlook.com>
2020-12-12 14:42:13 -0500

    Finished with Komodo Cafe challenge

--- 01_KomodoCafeConsole/ProgramUI.cs
+++ 01_KomodoCafeConsole/ProgramUI.cs
@@ -79,21 +79,58 @@ namespace _01_KomodoCafeConsole
 
         public void AddMealToMenu()
         {
+            Console.Clear();
+            Menu newMenuMeal = new Menu();
 
+            Console.WriteLine("Enter the new Meal Number:");
+            string mealNumAsString = Console.ReadLine();
+            int mealNumAsInt = int.Parse(mealNumAsString);
+            newMenuMeal.MealNumber = mealNumAsInt;
+
+            Console.WriteLine("\nEnter the new Meal Name:");
+            newMenuMeal.MealName = Console.ReadLine();
+
+            Console.WriteLine("\nEnter the new meal Description:");
+            newMenuMeal.Description = Console.ReadLine();
+
+            Console.WriteLine("\nEnter the new Meal Ingredients:");
+            newMenuMeal.ListOfIngredients = Console.ReadLine();
+
+            Console.WriteLine("\nEnter the new Meal Price:");
+            string priceAsString = Console.ReadLine();
+            double priceAsDouble = double.Parse(priceAsString);
+            newMenuMeal.Price = priceAsDouble;
+
+            _menu.AddMealToMenu(newMenuMeal);
         }
 
         public void DeleteMealFromMenu()
         {
+            DisplayMenu();
 
+            Console.WriteLine("Enter the Meal Number to delete that meal.");
+            string mealNumAsString = Console.ReadLine();
+            int mealNumAsInt = int.Parse(mealNumAsString);
+
+            bool wasDeleted = _menu.RemoveMealFromMenu(mealNumAsInt);
+
+            if (wasDeleted)
+            {
+                Console.WriteLine("\nThe meal was successfully deleted!");
+            }
+            else
+            {
+                Console.WriteLine("\nThe meal could not be deleted.");
+            }
         }
 
         private void SeedContentList()
         {
             Menu mealOne = new Menu(1, "Big Mac Meal", "Big Mac, Fries, and a Strawberry Shake", "Hamburger, cheese, lettuce, tomato, onion, and pickles. " +
                 "Golden brown fries. And freshly sourced strawberries and dairy for a deliciously tasting shake!", 5.99);
-            Menu mealTwo = new Menu(2, "Double Whopper Meal", "Double Whopper, Fries, and a Coke", "Double hamburger, cheese, lettuce, tomato, onion, and pickles." +
+            Menu mealTwo = new Menu(2, "Double Whopper Meal", "Double Whopper, Fries, and a Coke", "Double hamburger, cheese, lettuce, tomato, onion, and pickles. " +
                 "Golden brown fries. And a nice, always cold Coke", 6.99);
-            Menu mealThree = new Menu(3, "Triple Bacon Burger Meal", "For the meat lover in us all!", "Triple hamburger, 4 bacon slices, cheese, lettuce, tomato, onion, and pickles." +
+            Menu mealThree = new Menu(3, "Triple Bacon Burger Meal", "For the meat lover in us all!", "Triple hamburger, 4 bacon slices, cheese, lettuce, tomato, onion, and pickles. " +
                 "Golden brown tater tots. And a delicious Root Beer Float to wash it all down!", 9.99);
 
             _menu.AddMealToMenu(mealOne);
